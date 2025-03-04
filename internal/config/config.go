aeb501b6068c30f02b015777fcdf56f97820fa91
Jalad Shrimali <jaladshrimali18@gmail.com>
2025-03-04 15:42:12 +0530

    config file

---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Addr string
+}
+type Config struct {
+	Env         string                `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string                `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server "` //struct embedding
+}
+
+func MustLoad() *Config {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		flags := flag.String("config", "", "path to config file")
+		flag.Parse() //parse the flags
+
+		configPath = *flags //flag is a pointer
+		if configPath == "" {
+			log.Fatal("config path is not set")
+		}
+	}
+	if _, err := os.Stat(configPath); os.IsNotExist(err) { //check if the file exists
+		log.Fatalf("config file not found at %s", configPath)
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg) //load the config file
+	if err != nil {
+		log.Fatalf("failed to load config: %s", err.Error())
+	}
+	return &cfg
+}
